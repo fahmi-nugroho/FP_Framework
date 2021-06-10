7ec44077f3152092702319b64b2108347144f743
dwikiaditamasupangkat <dwikiaditamasupangkat@gmail.com>
2021-06-10 08:26:34 +0700

    perbaikan UI bacablog

--- application/views/OnlineShop/bacablog.php
+++ application/views/OnlineShop/bacablog.php
@@ -16,82 +16,55 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <div class="section-heading">
-              <h2>Sejarah Motif Sekar Jagad</h2>
+            <div class="section-heading bacablog">
+              <h1>Sejarah Motif Sekar Jagad</h1>
             </div>
           </div>
-          <div class="col-md-4">
-            <div class="right-image">
-              <img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="">
-            </div>
-          </div>
-          <div class="col-md-8">
-            <div class="left-content">
-              <h4>bagaimana sih Sejarah dari Motif Sekar Jagad??</h4>
-              <p>
-                  Incididunt mollit non nulla non dolor commodo nostrud ad occaecat amet reprehenderit ullamco. Nostrud voluptate amet duis reprehenderit id voluptate adipisicing ipsum adipisicing. Et et non consectetur fugiat amet do elit. Tempor irure cillum id et laborum aute consequat enim labore officia ipsum est velit. Duis esse mollit veniam consectetur minim duis nostrud.
-
-                  Id deserunt nulla elit ipsum. Occaecat officia fugiat ullamco exercitation. Sit ullamco proident in culpa id aliquip proident non reprehenderit eiusmod qui veniam irure. Culpa ipsum sit laboris occaecat fugiat. Laborum est dolore nisi fugiat enim cillum culpa enim voluptate velit qui aliquip Lorem anim. Ut ut fugiat nulla voluptate ut ea deserunt do occaecat eu labore sint ea.
-
-                  Laborum laborum nisi tempor laborum exercitation magna anim duis ut fugiat voluptate ut occaecat proident. Pariatur in fugiat irure nisi magna cillum enim ullamco veniam anim amet deserunt. In irure culpa labore consectetur ea cupidatat enim. Ea magna esse cupidatat exercitation sit non excepteur esse sunt magna. Ullamco irure consectetur nostrud Lorem cillum. Fugiat eiusmod nostrud officia culpa adipisicing ex sunt id proident laboris ad do nostrud. Non ut ex dolor ut officia do do cillum esse ipsum ipsum sunt enim sit.
-
-                  Irure cillum voluptate occaecat qui mollit in elit excepteur ullamco ullamco exercitation dolore. In excepteur sint aliquip amet mollit. Reprehenderit do incididunt laboris anim magna laboris. Sunt esse incididunt nostrud aliquip velit sit eu ex fugiat.
-
-                  Laborum anim anim pariatur amet est Lorem duis elit minim enim sit magna. Proident ullamco proident sunt laboris. Sint dolor ea occaecat ea ullamco occaecat id ex.
-
-                  Et Lorem adipisicing labore fugiat amet nisi irure labore nostrud. Velit aute eiusmod laboris aute nostrud quis enim velit ex consequat ipsum. Velit eiusmod laboris Lorem et labore voluptate aliquip nulla sint. Ex id proident voluptate nostrud laboris voluptate officia pariatur exercitation.
-
-                  Veniam pariatur sit adipisicing elit consequat commodo nulla irure voluptate deserunt veniam in ad. Consequat velit non elit dolor aliqua qui culpa qui est aute labore. Elit et mollit anim officia. Consectetur reprehenderit consequat ex veniam velit ut occaecat ex exercitation commodo commodo reprehenderit do. Consectetur ad fugiat proident aliquip culpa fugiat pariatur ipsum esse eiusmod. Aliquip ex tempor tempor pariatur.
-
-                  Occaecat laborum anim ea eiusmod dolor officia nostrud proident. Dolore cillum dolore culpa magna eiusmod incididunt cillum cillum voluptate enim amet aliqua. Sunt enim ipsum ex nostrud reprehenderit.
-
-                  Sit eiusmod eu nulla minim minim eiusmod tempor esse voluptate nisi labore ut nulla. Sit est est velit deserunt esse nisi nostrud. Do nisi non reprehenderit enim consectetur elit. Elit labore velit minim nostrud Lorem est culpa consectetur Lorem qui veniam.
-
-                  Esse do ea pariatur ea. Elit nostrud est voluptate esse commodo amet occaecat. Sit eu aliquip veniam enim incididunt enim. Ad reprehenderit tempor dolore sit sunt. Dolore qui id sunt ut laborum eu ipsum officia ut commodo consequat labore.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="team-members">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-heading">
-              <h2>Baca Artikel Lain</h2>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="team-member">
-              <div class="thumb-container">
-                <img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="">
-              </div>
-              <div class="down-content">
-                <h4>Teknik Memegang Canting</h4>
-                <p>Sit anim velit incididunt cupidatat proident esse sunt.</p>
+          <div class="col-md-9">
+            <div id="section" class="box">
+              <div id="content">
+                <ul class="articles box">
+                  <h4>Bagaimana sih sejarah dari motif sekar jagad?</h4>
+                  <li>
+                    <p><img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="" class="f-left" />
+                      Suspendisse posuere, metus eget pharetra adipiscing, arcu velit lobortis augue, quis pharetra mauris ante a velit. Duis feugiat, odio a mattis gravida, velit est euismod urna, vitae gravida elit turpis sit amet elit.
+                      Phasellus ac hendrerit tortor. Aliquam erat volutpat. Donec laoreet viverra sapien et luctus. Cras fringilla commodo nulla sit amet congue. Donec aliquam gravida elit, in fringilla urna adipiscing in. Sed vel risus id urna luctus eleifend.
+                      Morbi ut fringilla magna. Curabitur lobortis molestie tellus ac ultricies. Maecenas tempus rutrum mauris in auctor. Ut interdum diam a justo malesuada dignissim. Morbi blandit odio sed magna rhoncus tincidunt. Etiam diam neque, ornare in molestie posuere, vulputate a nisl.
+                      Donec dictum, erat vel varius ullamcorper, lorem ipsum vulputate eros, sit amet lacinia orci arcu ac mi. Cras pellentesque, lacus vel laoreet tristique, justo magna convallis ante, at pellentesque ligula sapien sit amet elit. Nulla ut nunc libero.
+                      Suspendisse posuere, metus eget pharetra adipiscing, arcu velit lobortis augue, quis pharetra mauris ante a velit. Duis feugiat, odio a mattis gravida, velit est euismod urna, vitae gravida elit turpis sit amet elit. Phasellus ac hendrerit tortor. Aliquam erat volutpat.
+                      Donec laoreet viverra sapien et luctus. Cras fringilla commodo nulla sit amet congue. Donec aliquam gravida elit, in fringilla urna adipiscing in. Sed vel risus id urna luctus eleifend. Morbi ut fringilla magna. Curabitur lobortis molestie tellus ac ultricies. Maecenas tempus rutrum mauris in auctor.
+                      Ut interdum diam a justo malesuada dignissim. Morbi blandit odio sed magna rhoncus tincidunt. Etiam diam neque, ornare in molestie posuere, vulputate a nisl. Donec dictum, erat vel varius ullamcorper, lorem ipsum vulputate eros, sit amet lacinia orci arcu ac mi.
+                      Cras pellentesque, lacus vel laoreet tristique, justo magna convallis ante, at pellentesque ligula sapien sit amet elit. Nulla ut nunc libero.
+                    </p>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
-          <div class="col-md-4">
-            <div class="team-member">
-              <div class="thumb-container">
-                <img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="">
+          <div class="col-md-3">
+            <div class="aside">
+              <div class="section-heading">
+                <h2>Artikel Baru</h2>
               </div>
-              <div class="down-content">
-                <h4>Motif Sekardangan</h4>
-                <p>Batik motif sekardangan merupakan motif pertama yang muncul dan dikenal dan hanya berwarna coklat, biru tua, dan jingga tua.</p>
+              <div class="sidenews">
+                <h6>beras kutah</h6>
+              <p class="box"> <img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="" class="f-left art" /> My name is Jessie Doe. I´m 26 years old and I´m living in the New York City.<br />
+                <a href="#">Read More...</a> </p>
               </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="team-member">
-              <div class="thumb-container">
-                <img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="">
+              <div class="sidenews">
+                <h6>beras kutah</h6>
+              <p class="box"> <img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="" class="f-left art" /> My name is Jessie Doe. I´m 26 years old and I´m living in the New York City.<br />
+                <a href="#">Read More...</a> </p>
               </div>
-              <div class="down-content">
-                <h4>Motif Kembang Tebu</h4>
-                <p>kembang tebu, artinya bunga tebu sebagai penggambaran Sidoarjo yang dulunya memiliki ratusan hektar perkebunan tebu sebagai bahan baku sejumlah pabrik gula.</p>
+              <div class="sidenews">
+                <h6>beras kutah</h6>
+              <p class="box"> <img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="" class="f-left art" /> My name is Jessie Doe. I´m 26 years old and I´m living in the New York City.<br />
+                <a href="#">Read More...</a> </p>
+              </div>
+              <div class="sidenews">
+                <h6>beras kutah</h6>
+              <p class="box"> <img src="<?php echo base_url() ?>assets/images/Batik/Batik Sidoarjo Motif Sekar Jagad.jpg" alt="" class="f-left art" /> My name is Jessie Doe. I´m 26 years old and I´m living in the New York City.<br />
+                <a href="#">Read More...</a> </p>
               </div>
             </div>
           </div>
